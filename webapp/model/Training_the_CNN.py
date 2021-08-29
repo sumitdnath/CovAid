2a12f6975b3301466957d41e08899940ebd44840
Sumit Debnath <sumitdebnath913@gmail.com>
2021-08-29 23:33:17 +0530

    Add files via upload

---
+++ webapp/model/Training_the_CNN.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D,Activation,MaxPooling2D
+from tensorflow.keras.utils import normalize
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras import Input
+from tensorflow.keras.callbacks import ModelCheckpoint
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+data=np.load('data.npy')
+target=np.load('target.npy')
+
+
+input_shape=data.shape[1:] #50,50,1
+inp=Input(shape=input_shape)
+convs=[]
+
+parrallel_kernels=[3,5,7]
+
+for k in range(len(parrallel_kernels)):
+
+    conv = Conv2D(128, parrallel_kernels[k],padding='same',activation='relu',input_shape=input_shape,strides=1)(inp)
+    convs.append(conv)
+
+out = Concatenate()(convs)
+conv_model = Model(inp, out)
+
+model = Sequential()
+model.add(conv_model)
+
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2,input_dim=128,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.summary()
+
+
+train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.1)
+
+checkpoint = ModelCheckpoint('model-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+history=model.fit(train_data,train_target,epochs=50,callbacks=[checkpoint],validation_split=0.1)
+
+
+plt.plot(history.history['loss'],'r',label='training loss')
+plt.plot(history.history['val_loss'],label='validation loss')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+plt.plot(history.history['accuracy'],'r',label='training accuracy')
+plt.plot(history.history['val_accuracy'],label='validation accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+print(model.evaluate(test_data,test_target))
